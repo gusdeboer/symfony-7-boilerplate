140ddbcc6c09f1846ce4eac83dd76d53dd7c210c
Gus de Boer <gusdeboer93@gmail.com>
2024-07-01 21:58:40 +0200

    feat(2): start project settings

---
+++ src/Controller/SettingController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Setting;
+use App\Form\SettingType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/settings')]
+class SettingController extends AbstractController
+{
+    #[Route('/', name: 'app_settings', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $repository = $entityManager->getRepository(Setting::class);
+        $settings = $repository->findAll();
+
+        $form = $this->createForm(SettingType::class, null, ['settings' => $settings]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            foreach ($settings as $setting) {
+                $key = $setting->getKey();
+                $value = $form->get($key)->getData();
+                $setting->setValue($value);
+                $setting->setUpdatedAt(new \DateTimeImmutable());
+            }
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_settings', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('setting/edit.html.twig', [
+            'setting' => $settings,
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/Form/SettingType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Setting;
+use App\Types\SettingsValueType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Webmozart\Assert\Assert;
+
+class SettingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        Assert::allIsInstanceOf($options['settings'], Setting::class);
+
+        foreach ($options['settings'] as $setting) {
+            $type = match ($setting->getType()) {
+                SettingsValueType::String => null,
+                SettingsValueType::Integer => NumberType::class,
+                SettingsValueType::Boolean => CheckboxType::class,
+                SettingsValueType::Json => TextType::class,
+                SettingsValueType::Date => DateType::class,
+                default => throw new \InvalidArgumentException(sprintf('Unsupported setting type "%s"', $setting->getType()))
+            };
+
+            $options = match ($setting->getType()) {
+                SettingsValueType::String => [],
+                SettingsValueType::Integer => ['html5' => true],
+                SettingsValueType::Boolean => [],
+                SettingsValueType::Json => [],
+                SettingsValueType::Date => ['html5' => true],
+                default => throw new \InvalidArgumentException(sprintf('Unsupported setting type "%s"', $setting->getType()))
+            };
+
+            $value = $setting->getValue();
+
+            if (SettingsValueType::Boolean === $setting->getType()) {
+                $value = '1' === $setting->getValue();
+            }
+
+            if (SettingsValueType::Date === $setting->getType() && is_string($setting->getValue())) {
+                $value = new \DateTime($setting->getValue());
+            }
+
+            $builder->add($setting->getKey(), $type, [
+                'label' => sprintf('settings.%s', $setting->getKey()),
+                'data' => $value,
+                'required' => false,
+            ] + $options);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'settings' => null,
+        ]);
+    }
+}
